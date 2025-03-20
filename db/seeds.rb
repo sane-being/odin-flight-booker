521475948b5607e416ba628d28b9096de42b1b6e
shrey <117995509+sane-being@users.noreply.github.com>
2025-03-20 14:21:38 +0000

    add seed data for Flights
    - added logic to create seed data to create:
    15 random flights between random two airports, in upcoming 31 days
    in db/seeds.rb
    bin/rails db:seed
    
    improvement:
    - rounded off journey_duration to 1 decimal place
    in app/views/flights/_flight.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,17 @@ airports_list = [ "ATL", "PEK", "DXB", "LAX", "HND", "LHR", "ORD", "CDG", "DFW",
 airports_list.each do |airport_code|
   Airport.find_or_create_by!(airport_code: airport_code)
 end
+
+airports_id_list = Airport.pluck(:id)
+datetime_range = (Time.now..(Time.now + 60*60*24*31)) # upcoming 31 days
+duration_range = (60*60..60*60*24)  # 1 hour to 24 hours
+15.times do
+  two_random_airports = airports_id_list.sample(2)
+  dep_date = rand(datetime_range)
+  Flight.find_or_create_by!(
+    departure_datetime: dep_date,
+    arrival_datetime: dep_date + rand(duration_range),
+    departure_airport_id: two_random_airports[0],
+    arrival_airport_id: two_random_airports[1]
+  )
+end
